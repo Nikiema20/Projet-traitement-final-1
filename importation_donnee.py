ee660c775343b50e0a2f1942637952b011007c0a
Nikiema20 <zoukou1996@gmail.com>
2021-04-23 21:15:51 +0200

    changer bas
    modifier base

---
+++ importation_donnee.py
@@ -0,0 +1,30 @@
+import csv ;  # importer le module csv de python
+import json ; # Importer le module json
+class Importation_donnee :
+    def __init__(self,folder, file_name):
+        self.folder=folder
+        self.file_name=file_name
+
+    def import_csv(self):
+               
+        data = []
+        with open(self.folder + self.file_name, encoding='ISO-8859-1') as csvfile :
+            covidreader = csv.reader(csvfile, delimiter= ';')
+            for row in covidreader :
+                data.append(row)
+        return data
+        
+    def import_json(self):
+        # Dossier où se trouve le fichier :
+        data = []
+        with open(self.folder + self.file_name) as json_file :
+            covidreader = json.load(json_file)
+            for row in  covidreader:
+                data.append(row)
+            return data
+
+        
+        
+            
+       
+       
---
+++ main.py
@@ -0,0 +1,56 @@
+from donnee import Donnees
+from descriptive import MethodesDescriptives
+from graphique import Graphique
+from clustering import Clustering
+
+
+if __name__ == "__main__":
+   # a=Don
+    # importation des donnes hopitaliere
+    covid_hospit=Donnees('P:\\Projet_de_Traitemant\\Projet\\',"donnees_hospitalieres_covid.csv")
+    covid_hospit.import_csv()
+    print(*covid_hospit.import_csv(),sep="\n")
+    # importation des données donnees_hospitalieres_classe_age_covid
+    #covid_hospit_class=Donnees('P:\\Projet_de_Traitemant\\Projet\\',"donnees_hospitalieres_classe_age_covid.csv")
+   # print(*covid_hospit_class.import_csv(),sep="\n")
+    
+     # importation des données hospitalieres_etablissements_covid
+  #  covid_hospit_etab=Donnees('P:\\Projet_de_Traitemant\\Projet\\',"donnees_hospitalieres_etablissements_covid.csv")
+  #  print(*covid_hospit_etab.import_csv(),sep="\n")
+    
+    
+    # importation des données hospitalieres_nouveaux_covid
+  #  covid_hospit_nouv=Donnees('P:\\Projet_de_Traitemant\\Projet\\',"donnees_hospitalieres_nouveaux_covid.csv")
+  #  print(*covid_hospit_nouv.import_csv(),sep="\n")
+    
+    a= MethodesDescriptives(covid_hospit.import_csv())
+   
+    print(a.moyenne('hosp'))
+   
+    print(a.nombre_totale('rea'))
+    
+   # d= MethodesDescriptives(covid_hospit_class.import_csv())
+   # print(d.moyenne('hosp'))
+    
+   # print(d.nombre_totale('rea'))
+    print(a.total_vacance("2021-01-01","2021-01-31","jour","hosp"))
+    # vacance
+    #print(a.total_vacance("2021-02-03","2021-03-03","jour","hosp"))
+   # print(total_vacance_specifique("2021-02-03","2021-03-03","jour","hosp","76"))
+    #c=Clustering(a)
+   #"c.donnee_centre
+    #b=Graphique(a)
+    #b.boxplot('hosp','rea')
+    
+    #a.affichageRegion("hosp")
+   # covid_hospit=Donnees('P:\\Projet_de_Traitemant\\Projet\\',"donnees_hospitalieres_covid.csv")
+   # print(*covid_hospit.import_csv(),sep="\n")
+ 
+    #  affichage(covid_region)
+   
+
+    
+ #   for row in read : # parcours du lecteur avec une boucle
+  #  print (row) # affichage ligne à ligne
+
+#file.close ( )
