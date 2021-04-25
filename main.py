479a52fabe79864b374f79d8e322f0e5187287c4
fearthebeard225 <82213389+fearthebeard225@users.noreply.github.com>
2021-04-25 13:49:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ from table import Table
 
 
 if __name__ == "__main__":
+  # creation de la table covid_hospit_incid_reg qui est une instance de la classe Table
+  fich = open("covid_hospit_incid_reg.csv", "r")
+  lect = csv.reader( fich, delimiter = ';')
+  list_lignes_covid_hospit_incid_reg=[]
+  for row in lect: 
+    ligne = []
+    annee =int( row[0][0:4]) # on retient l'année dans l'affichage de la date
+    mois = int(row[0][5:7])
+    jour= int(row[0][8:10])
+    date_ligne= date(annee , mois , jour)
+    ligne.append(date_ligne)
+    ligne.append(row[1])
+    ligne.append(row[2])
+    ligne.append(int(row[3]))
+    list_lignes_covid_hospit_incid_reg.append(ligne)
+  fich.close
+  
+
+  nom_colonnes = ["jour", "nomReg", "numReg", "incid_rea"]
+
+  table_covid_hospit_incid_reg = Table(nom_colonnes , list_lignes_covid_hospit_incid_reg)
+   
    # importation de la table donnees_hospitalieres_classe_age_covid
    list_lignes_donnees_hospitalieres_classe_age_covid=[]
    fich1 = open("donnees_hospitalieres_classe_age_covid.csv","r")
