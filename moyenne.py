65e6ac637d0f6fd8880d1644eac47d7b232c6cd3
slm5e <82213509+slm5e@users.noreply.github.com>
2021-04-27 13:22:51 +0200

    rajout de self

--- moyenne.py
+++ moyenne.py
@@ -3,7 +3,7 @@ import datetime as d
 from table import Table
 
 class Moyenne(EstimateurTable):
-    def traiter_table(table, colonne): #colonne est une liste contenant la colonne sur laquelle on fait la moyenne
+    def traiter_table(self, table, colonne): #colonne est une liste contenant la colonne sur laquelle on fait la moyenne
         mean=0
         for i in range(len(colonne)):
             mean+=float(colonne[i])
--- variance.py
+++ variance.py
@@ -2,7 +2,7 @@ from estimateur_table import EstimateurTable
 from table import Table
 
 class Variance(EstimateurTable):
-    def traiter_table(table, nom_colonne): #colonne est une liste contenant la colonne sur laquelle on fait la moyenne
+    def traiter_table(self, table, nom_colonne): #colonne est une liste contenant la colonne sur laquelle on fait la moyenne
         mean=0
         for i in range(len(nom_colonne)):
             mean+=float(nom_colonne[i])
