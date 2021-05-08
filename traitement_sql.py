d69ff8705d2a6302ff6298ce448cb82b38b757a6
fearthebeard225 <82213389+fearthebeard225@users.noreply.github.com>
2021-05-08 22:22:52 +0200

    Update traitement_sql.py

--- traitement_sql.py
+++ traitement_sql.py
@@ -1,7 +1,53 @@
 import sqlite3 
 bases = sqlite3.connect(':memory:')
-from datetime import date
 
+import csv
+
+import travail.estimateur_table 
+import travail.operation_de_tables
+import travail.transformation_table
+import travail.get_colonne
+import travail.importation_donnee
+
+import travail.structuration_donnees
+import travail.table
+import travail.moyenne
+
+import travail.transformation_spatiale
+import travail.transformation_temporelle
+from datetime import date
+from travail.table import Table
+from travail.get_colonne import GetColonnes
+from travail.transformation_temporelle import TransformationTemporelle
+from travail.importation_donnee import Importation_donnee
+from travail.transformation_spatiale import TransformationSpatiale
+from travail.moyenne import Moyenne
+from travail.aggregation import Aggregation
+#from graphique import Graphique
+#from clustering import Clustering
+
+from travail.structuration_donnees import list_lignes_covid_hospit_incid_reg
+from travail.structuration_donnees import nom_colonnes_covid_hospit_incid_reg
+from travail.structuration_donnees import nom_colonnes_donnees_hospitalieres_classe_age_covid
+from travail.structuration_donnees import list_lignes_donnees_hospitalieres_classe_age_covid
+from travail.structuration_donnees import list_lignes_donnees_hospitalieres_covid
+from travail.structuration_donnees import nom_colonnes_donnees_hospitalieres_covid
+from travail.structuration_donnees import nom_colonnes_donnees_hospitalieres_etablissements_covid
+from travail.structuration_donnees import nom_colonnes_donnees_hospitalieres_nouveaux_covid
+from travail.structuration_donnees import list_lignes_donnees_hospitalieres_etablissements_covid
+from travail.structuration_donnees import list_lignes_donnees_hospitalieres_nouveaux_covid 
+
+table_covid_hospit_incid_reg = Table(nom_colonnes_covid_hospit_incid_reg , list_lignes_covid_hospit_incid_reg)
+table_donnees_hospitalieres_classe_age_covid = Table(nom_colonnes_donnees_hospitalieres_classe_age_covid, list_lignes_donnees_hospitalieres_classe_age_covid)
+  
+  # importation de la table donnees_hospitalieres_classe_age_covid
+  
+  # creation de la table donnees_hospitalieres_covid
+  
+
+table_donnees_hospitalieres_covid = Table(nom_colonnes_donnees_hospitalieres_covid, list_lignes_donnees_hospitalieres_covid)
+table_donnees_hospitalieres_etablissements_covid = Table(nom_colonnes_donnees_hospitalieres_etablissements_covid , list_lignes_donnees_hospitalieres_etablissements_covid)
+table_donnees_hospitalieres_nouveaux_covid = Table(nom_colonnes_donnees_hospitalieres_nouveaux_covid , list_lignes_donnees_hospitalieres_nouveaux_covid)
 v = bases.cursor()
 v.execute("""CREATE TABLE covid_hospit_incid_reg (jour text , nom_region text , numero_region integer , reanimation integer)""")
 l_covid_hospit_incid_reg= []
@@ -34,13 +80,13 @@ l_donnees_hospitalieres_covid =[]
 for ligne in table_donnees_hospitalieres_covid.lignes:
     vrai_ligne=(ligne[0],ligne[1],str(ligne[2]),ligne[3], ligne[4],ligne[5],ligne[6])
     l_donnees_hospitalieres_covid.append(vrai_ligne) 
-  v.executemany("INSERT INTO donnees_hospitalieres_covid VALUES (?,?,?,?,?,?,?)", l_donnees_hospitalieres_covid)
+v.executemany("INSERT INTO donnees_hospitalieres_covid VALUES (?,?,?,?,?,?,?)", l_donnees_hospitalieres_covid)
 
   # creation de la table donnees_hospitalieres_etablissements_covid
 
 v.execute("""CREATE TABLE donnees_hospitalieres_etablissements_covid(numero_department text , jour text, nombre integer) """)
-  l_donnees_hospitalieres_etablissements_covid =[]
-  for ligne in table_donnees_hospitalieres_etablissements_covid.lignes:
+l_donnees_hospitalieres_etablissements_covid =[]
+for ligne in table_donnees_hospitalieres_etablissements_covid.lignes:
     vrai_ligne = (ligne[0],str(ligne[1]),ligne[2])
     l_donnees_hospitalieres_etablissements_covid.append(vrai_ligne)
 v.executemany("INSERT INTO donnees_hospitalieres_etablissements_covid VALUES (?,?,?)", l_donnees_hospitalieres_etablissements_covid)
