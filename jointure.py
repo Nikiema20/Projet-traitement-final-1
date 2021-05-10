7205894d502f8b8b0d1e77f1e21e85ed54b1e9dc
fearthebeard225 <82213389+fearthebeard225@users.noreply.github.com>
2021-05-10 11:28:54 +0200

    Update jointure.py

--- jointure.py
+++ jointure.py
@@ -5,11 +5,11 @@ from travail.traitement_sql import v
 from abc import ABC 
 
 class Jointure(ABC) :
-    def __init__ (self, nature):# la jointure est "inner", "outer"
+    def __init__ (self, nature):# la jointure est "inner", "full outer", "left outer" ...
         self.nature = nature # nature est en str
         
     
     def joindre(self, nom_table1 , nom_table2 , nom_colonne): # nom_table1 et nom_table2 son des str 
-        text = "SELECT * FROM {} {} JOIN {}  USING({} , jour)".format(nom_table1,self.nature, nom_table2 , nom_colonne )  # exactement la syntaxe SQL pour une jointure
+        text = "SELECT * FROM {}  {} JOIN {}  USING({} , jour)".format(nom_table1,self.nature, nom_table2 , nom_colonne )  # exactement la syntaxe SQL pour une jointure
         return(text)
   
