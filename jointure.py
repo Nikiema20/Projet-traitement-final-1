60ac4f37dc7599cc9a862c733aa309a34a4fd791
fearthebeard225 <82213389+fearthebeard225@users.noreply.github.com>
2021-05-10 11:15:22 +0200

    Update jointure.py

--- jointure.py
+++ jointure.py
@@ -5,7 +5,7 @@ class Jointure :
     
     def joindre(self, nom_table1 , nom_table2 , nom_colonne): # nom_table1 et nom_table2 son des str 
         text = "SELECT * FROM {} {} JOIN {}  USING({} , jour)".format(nom_table1,self.nature, nom_table2 , nom_colonne )  # exactement la syntaxe SQL pour une jointure
-    return(text)
+        return(text)
         
         
         
