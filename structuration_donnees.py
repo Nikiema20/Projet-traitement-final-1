f3c7806473578d0401504692c97538d841a2ced4
fearthebeard225 <82213389+fearthebeard225@users.noreply.github.com>
2021-05-12 14:04:58 +0200

    Update structuration_donnees.py

--- structuration_donnees.py
+++ structuration_donnees.py
@@ -5,22 +5,33 @@ import json
 #from graphique import Graphique
 #from clustering import Clustering
 
-fich = open("covid_hospit_incid_reg.csv", "r")
+# Le but de ce code est de créer des instances de la classe Table qu'on a crée nous memes)
+# Cependant, la création d'une instance Table requiert une liste qui contiendra les noms(libéllés) des colonnes et une liste qui contiendra chaque ligne sous forme de liste
+
+# L'idée est donc d'extraire pour une table CSV recue, toutes ces informations
+
+
+# Par exemple, pour la première table CSV du nom de "covid_hospit_incid_reg.csv":
+fich = open("covid_hospit_incid_reg.csv", "r") # on l'ouvre en mode lecture
 lect = csv.reader( fich, delimiter = ';')
-list_lignes_covid_hospit_incid_reg=[]
-for row in lect: 
-    ligne = []
+list_lignes_covid_hospit_incid_reg=[] # on crée la liste qui contiendra toutes les lignes de cette table CSV;  chaque ligne sera aussi une liste qui contiendra les valeurs de chaque variable
+# on passe au remplissage chaque ligne
+for row in lect: # pour chaque ligne dans la table CSV
+    ligne = [] # On crée la liste qui représentera cette ligne
+    # il faut noter que dans cette table, la date est située à la première colonne
     annee =int( row[0][0:4]) # on retient l'année dans l'affichage de la date
-    mois = int(row[0][5:7])
-    jour= int(row[0][8:10])
-    date_ligne= date(annee , mois , jour)
-    ligne.append(date_ligne)
-    ligne.append(row[1])
+    mois = int(row[0][5:7])# on retient le mois
+    jour= int(row[0][8:10])# on retient le jour
+    date_ligne= date(annee , mois , jour) # on en crée une instance de la classe date du module date.time
+    ligne.append(date_ligne)# on remplit le premier élément de la liste 
+    # on conclut par remplir les autres éléments selon leurs classes naturelles
+    ligne.append(row[1]) 
     ligne.append(row[2])
     ligne.append(int(row[3]))
-    list_lignes_covid_hospit_incid_reg.append(ligne)
+    list_lignes_covid_hospit_incid_reg.append(ligne) # on rajoute cette ligne à la liste des ligne
+    # par exemple, après sa création une ligne sera représentée comme :  [ datetime.date(2020,10,2), "Aquitaine" , "1" , 12]
 fich.close()
-nom_colonnes_covid_hospit_incid_reg = ["jour", "nom_region", "numero_region", "reanimation"]
+nom_colonnes_covid_hospit_incid_reg = ["jour", "nom_region", "numero_region", "reanimation"] # est la liste des variables de la table de départ
 
 
 # creation table 2 
