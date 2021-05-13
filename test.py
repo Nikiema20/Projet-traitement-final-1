76b7d372294bc9080df14fa48cb0bc13734f06a1
slm5e <82213509+slm5e@users.noreply.github.com>
2021-05-13 13:34:16 +0200

    last1

--- test.py
+++ test.py
@@ -8,15 +8,15 @@ class JeuxTest(unittest.TestCase):
  
     def test_importe_classe(self): 
         test1 = jd.Importation_donnee('P:/Projet_de_Traitemant/projet 8/Projet-traitement-final-1/','covid_hospit_incid_reg.csv') 
-        #test2 = jd.Importation_donnee('C:/Users/georg/OneDrive/Documents/GitHub/ProjetDonnees/Bases_de_donnees/donnees-hospitalieres-classe-age-covid19-2021-03-03-17h03.csv') 
+    
         self.assertIsInstance(test1, jd.Importation_donnee) 
-         #self.assertIsInstance(test2.importer(),dn.Donnees) 
+         
          
     def test_import_donnee(self): 
          test1 = jd.Importation_donnee('P:/Projet_de_Traitemant/projet 8/Projet-traitement-final-1/','covid_hospit_incid_reg.csv') 
-         #test2 = jd.Jeux_donnees('C:/Users/georg/OneDrive/Documents/GitHub/ProjetDonnees/Bases_de_donnees/donnees-hospitalieres-classe-age-covid19-2021-03-03-17h03.csv') 
+          
          self.assertTrue(test1.import_csv()) 
-        # self.assertTrue(test2.importer().type_fichier == "csv") 
+   
           
     def test_lien_fichier(self): 
         test1 = jd.Importation_donnee('P:/Projet_de_Traitemant/projet 8/Projet-traitement-final-1/','VacancesScolaires.json') 
